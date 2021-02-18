a462bc55e8defa4f8bc76867a7928d29f71a1c4e
Deepak <yourdeep88@gmail.com>
2021-02-18 14:53:29 +0530

    redis initialized

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,4 +1,9 @@
 var fetch = require("node-fetch");
+var redis = require("redis"),
+  client = redis.createClient();
+
+const { promisify } = require("util");
+const setAsync = promisify(client.set).bind(client);
 
 const baseURL = "https://jobs.github.com/positions.json";
 async function fetchGithub() {
@@ -17,8 +22,28 @@ async function fetchGithub() {
   }
 
   console.log("all jobs = " + allJobs.length);
-}
 
-fetchGithub();
+  // filter algo
+  const jrJobs = allJobs.filter((job) => {
+    const jobTitle = job.title.toLowerCase();
+    // algo logic
+    if (
+      jobTitle.includes("senior") ||
+      jobTitle.includes("manager") ||
+      jobTitle.includes("sr.") ||
+      jobTitle.includes("architect")
+    ) {
+      return false;
+    }
+    return true;
+  });
+
+  console.log("filtered down to", jrJobs.length);
+
+  // set in redis
+  const success = await setAsync("github", JSON.stringify(jrJobs));
+
+  console.log({ success });
+}
 
 module.exports = fetchGithub;
